d638ad999f6b88e86560c61a84dd278d1ace887f
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-03-25 11:33:20 +0100

    tal på test

--- src/Opgave1.java
+++ src/Opgave1.java
@@ -3,8 +3,8 @@ public class Opgave1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("test");
-		System.out.print("test");
+		System.out.print("test 1");
+		System.out.print("test 2");
 	}
 
 }
