576433e81aed260e4f2c363cecb5bc688d4c3d69
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-03-25 13:55:37 +0100

    Denne tester på klassen complex
    Den burde hedde opgave 2

--- src/Opgave1.java
+++ src/Opgave1.java
@@ -3,8 +3,31 @@ public class Opgave1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("test 1");
-		System.out.print("test 2");
+		Complex z0 = new Complex();
+		Complex z1 = new Complex(1,2);
+		Complex z11 = new Complex(1,1);
+		Complex z2 = new Complex(4.3,5.2);
+//		
+//		System.out.println(z0.toString());
+//		System.out.println(z1.toString());
+//		System.out.println(z11.toString());
+//		System.out.println(z2.toString());
+//		
+//		
+//		System.out.println(z1.abs());
+//		System.out.println(z11.abs());
+		System.out.println(z2.times(z1));
+		Complex z = (z2.plus(z1));
+		System.out.println(z);
+		System.out.println(z2.plus(z1).getRe());
+		System.out.println(z2.plus(z1).getIm());
+		System.out.println(z2.plus(z1).getRe());
+		System.out.println(z2.plus(z1).getIm());
+		System.out.println(z2.plus(z1).getRe());
+		System.out.println(z2.plus(z1).getIm());
+//		System.out.println(z2.getRe());
+//		System.out.println(z2.getIm());
+		
 	}
 
 }
