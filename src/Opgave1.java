1c8ee95b7423d837645494118a1db819c5499682
Mads Frimann Madsen <mfrimannm@gmail.com>
2014-03-31 23:07:34 +0200

    begyndt på iterate

--- src/Opgave1.java
+++ src/Opgave1.java
@@ -1,33 +1,25 @@
-
 public class Opgave1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Complex z0 = new Complex();
-		Complex z1 = new Complex(1,2);
-		Complex z11 = new Complex(1,1);
-		Complex z2 = new Complex(4.3,5.2);
-//		
-//		System.out.println(z0.toString());
-//		System.out.println(z1.toString());
-//		System.out.println(z11.toString());
-//		System.out.println(z2.toString());
-//		
-//		
-//		System.out.println(z1.abs());
-//		System.out.println(z11.abs());
-		System.out.println(z2.times(z1));
-		Complex z = (z2.plus(z1));
-		System.out.println(z);
-		System.out.println(z2.plus(z1).getRe());
-		System.out.println(z2.plus(z1).getIm());
-		System.out.println(z2.plus(z1).getRe());
-		System.out.println(z2.plus(z1).getIm());
-		System.out.println(z2.plus(z1).getRe());
-		System.out.println(z2.plus(z1).getIm());
-//		System.out.println(z2.getRe());
-//		System.out.println(z2.getIm());
+	static int MAX = 20;
 		
+	public static void main(String[] args) {
+
+		Complex z0 = new Complex(4, 2);
+		System.out.println(z0.toString());
+		System.out.println("begin iterate");
+		int max = iterate(z0);
+		System.out.println("max " + max);
 	}
 
+	public static int iterate(Complex z0) {
+		Complex z = new Complex(z0);
+		System.out.println(z.toString());
+		for (int i = 0; i < MAX; i++) {
+			if (z.abs() > 2.0) {
+				return i;
+			}
+			z = z.times(z).plus(z0);
+		}
+		return MAX;
+	}
 }
